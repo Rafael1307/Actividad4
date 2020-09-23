b35191ace79f8cf9213d89de9f863f500bb0d38f
Rafael Rodriguez <rafael.rmartinez@alumnos.udg.mx>
2020-09-22 23:36:54 -0500

    Funciona la funcion para decir el signo zodiacal

--- Actividad4.py
+++ Actividad4.py
@@ -22,3 +22,94 @@ def operaciones():
 	else:
 		print("Opcion invalida")
 	return 0
+
+def signos():
+	mes = input("Ingresa tu mes de nacimiento: ")
+	dia = int(input("Ingresa tu dia de nacimiento: "))
+	if mes == "enero":
+		if dia > 0 and dia <= 20:
+			print("Tu signo es Capricornio")
+		elif dia > 20 and dia <= 31:
+			print("Tu signo es Acuario")
+		else:
+			print("Dia incorrecto")
+	elif mes == "febrero":
+		if dia > 0 and dia <= 19:
+			print("Tu signo es Acuario")
+		elif dia > 19 and dia <= 31:
+			print("Tu signo es Piscis")
+		else:
+			print("Dia incorrecto")
+	elif mes == "marzo":
+		if dia > 0 and dia <= 20:
+			print("Tu signo es Piscis")
+		elif dia > 20 and dia <= 31:
+			print("Tu signo es Aries")
+		else:
+			print("Dia incorrecto")
+	elif mes == "abril":
+		if dia > 0 and dia <= 20:
+			print("Tu signo es Aries")
+		elif dia > 20 and dia <= 31:
+			print("Tu signo es Tauro")
+		else:
+			print("Dia incorrecto")
+	elif mes == "mayo":
+		if dia > 0 and dia <= 20:
+			print("Tu signo es Tauro")
+		elif dia > 20 and dia <= 31:
+			print("Tu signo es Geminis")
+		else:
+			print("Dia incorrecto")
+	elif mes == "junio":
+		if dia > 0 and dia <= 2:
+			print("Tu signo es Geminis")
+		elif dia > 21 and dia <= 31:
+			print("Tu signo es Cancer")
+		else:
+			print("Dia incorrecto")
+	elif mes == "julio":
+		if dia > 0 and dia <= 22:
+			print("Tu signo es Cancer")
+		elif dia > 22 and dia <= 31:
+			print("Tu signo es Leo")
+		else:
+			print("Dia incorrecto")
+	elif mes == "agosto":
+		if dia > 0 and dia <= 23:
+			print("Tu signo es Leo")
+		elif dia > 23 and dia <= 31:
+			print("Tu signo es Virgo")
+		else:
+			print("Dia incorrecto")
+	elif mes == "septiembre":
+		if dia > 0 and dia <= 22:
+			print("Tu signo es Virgo")
+		elif dia > 22 and dia <= 31:
+			print("Tu signo es Libra")
+		else:
+			print("Dia incorrecto")
+	elif mes == "octubre":
+		if dia > 0 and dia <= 22:
+			print("Tu signo es Cancer")
+		elif dia > 22 and dia <= 31:
+			print("Tu signo es Leo")
+		else:
+			print("Dia incorrecto")
+	elif mes == "noviembre":
+		if dia > 0 and dia <= 22:
+			print("Tu signo es Leo")
+		elif dia > 22 and dia <= 31:
+			print("Tu signo es Virgo")
+		else:
+			print("Dia incorrecto")
+	elif mes == "diciembre":
+		if dia > 0 and dia <= 21:
+			print("Tu signo es Virgo")
+		elif dia > 21 and dia <= 31:
+			print("Tu signo es Libra")
+		else:
+			print("Dia incorrecto")
+	else:
+		print("Mes incorrecto")
+	return 0
